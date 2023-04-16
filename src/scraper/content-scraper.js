387eaab8e75b11b1e54357c5da129b97d50d8f2d
s.zheng <sophia.ffz@gmail.com>
2023-04-16 16:10:04 -0700

    Update scraper

--- src/scraper/content-scraper.js
+++ src/scraper/content-scraper.js
@@ -1,7 +1,32 @@
 const { Builder, By } = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
 require('events').EventEmitter.defaultMaxListeners = 0
-const problems = require('../assets/prob.json')
+const problems = require('./q1.json')
+
+// const problems = {
+//   "1": {
+//     "id": 1,
+//     "title": "Two Sum",
+//     "url": "https://leetcode.com/problems/two-sum",
+//     "difficulty": 1,
+//     "paidOnly": 0,
+//     "favor": 0,
+//     "solved": 0,
+//     "lastSubmit": "2000-01-16T00:00:00.000Z",
+//     "noteUrl": ""
+//   },
+//   "2": {
+//     "id": 2,
+//     "title": "Add Two Numbers",
+//     "url": "https://leetcode.com/problems/add-two-numbers",
+//     "difficulty": 2,
+//     "paidOnly": 0,
+//     "favor": 0,
+//     "solved": 0,
+//     "lastSubmit": "2000-01-16T00:00:00.000Z",
+//     "noteUrl": ""
+//   },
+// }
 
 const fs = require('fs')
 
@@ -13,6 +38,7 @@ const fetchAll = async (problems) => {
 
   for (let [id, url, premium] of urls) {
     if (!premium) {
+      console.log(`scrapping question: ${id}`)
       await scraper(id, url)
     }
   }
@@ -24,16 +50,18 @@ const scraper = async (id, url) => {
   await browser.get(url)
 
   // Get question content
+  // const data = await browser.wait(browser.findElement(By.className("_1l1MA")), 4000)
+
   const data = await browser.wait(browser.findElement(By.className("_1l1MA")), 4000)
 
-  const content = await data.getText()
+  const content = await data.getAttribute('innerHTML')
   browser.close()
   map[id] = content
 }
 
 fetchAll(problems).then(() => {
   fs.writeFile(
-    'contents.json',
+    'content1.json',
     JSON.stringify(map),
     'utf8',
     () => console.log('Done!')
